b93bd6fe3eb2543d53f6b79e9fd19740db0fd3f8
gebiWangshushu <389483637@qq.com>
2018-11-27 11:51:48 +0800

    第一次提交，apollo项目

---
+++ WebApi.Hei.Apollo/WebApi.Hei.Apollo/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Com.Ctrip.Framework.Apollo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApi.Hei.Apollo.Controllers
+{
+    [Route("api/[controller]/{key}")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IConfiguration _configuration;
+        public ValuesController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get(string key)
+        {
+            var value = _configuration.GetValue<string>(key);
+            return Ok($"key:{key} value:{value}");
+        }
+    }
+}
---
+++ WebApi.Hei.Apollo/WebApi.Hei.Apollo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Com.Ctrip.Framework.Apollo;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Hei.Apollo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Run();
+        }
+
+        public static IWebHost CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, builder) =>
+                {
+                    builder
+                    .AddApollo(builder.Build().GetSection("apollo"))
+                    .AddDefault() //默认的application Namespace
+                    .AddNamespace("TEST2.Hei.Global")
+                    .AddNamespace("TEST1.Hei.Public") //public 类型的Namespace
+                    .AddNamespace("Hei.Private");//private 类型的Namespace
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
